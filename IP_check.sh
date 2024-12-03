7da2e308b9a5966d707784638cc3fe73bea02f89
chanan shenker <chananshenker1@gmail.com>
2024-12-03 20:41:58 +0200

    Update IP_check.sh
    - added level testing for more and less malicious addresses. 
    - added colours.
    - made out more clean for bigger ranged scans

--- IP_check.sh
+++ IP_check.sh
@@ -9,66 +9,94 @@ RESET="\e[0m"
 VIRUSTOTAL_API_KEY='<YOUR_API_KEY_HERE>'
 ABUSEIPDB_API_KEY='<YOUR_API_KEY_HERE>'
 GREAYNOISE_API_KEY='<YOUR_API_KEY_HERE>'
+FRAUDGUARD_CREDS='<YOUR_API_KEY_HERE>'
 
 INPUT="$1"
 
 function IP_FILE(){
       for IP in $(cat "$INPUT");do
-            echo -e "\n${CYAN}results for ${IP}:${RESET}\n\nVirusTotal:"
-            VT_SCORE=$(curl -s --request GET \
+            echo -e "\n${CYAN}results for ${IP}:${RESET}\n"
+            VT_RES_PARSED=$(curl -s --request GET \
             --url "https://www.virustotal.com/api/v3/ip_addresses/${IP}" \
-            --header "x-apikey: ${VIRUSTOTAL_API_KEY}" | jq '.data.attributes.last_analysis_stats' | grep -Ev '\{|\}' |sed 's/"//g ; s/,//g')
-            echo -e "${VT_SCORE}"
+            --header "x-apikey: ${VIRUSTOTAL_API_KEY}" | jq '.data.attributes.last_analysis_stats' | grep -vE '{|}' | sed 's/\,//g ; s/\"//g')
+            if [ "$(echo "$VT_RES_PARSED" | grep -E 'malicious|suspicious' | awk '{print $2}' | sort | uniq)" != '0' ];then
+                  echo -e "VirusTotal: ${RED}NOT CLEAN!\n${VT_RES_PARSED}${RESET}" 
+            else 
+                  echo -e "VirusTotal: ${GREEN}clean :)${RESET}"
+            fi
 
-            echo -e "\nAbuseIPDB:"
-            AID_SCORE=$(curl -s -G https://api.abuseipdb.com/api/v2/check \
+            AID_RES=$(curl -s -G https://api.abuseipdb.com/api/v2/check \
             --data-urlencode "ipAddress=${IP}" \
             -H "Key: ${ABUSEIPDB_API_KEY}" \
-            -H "Accept: application/json" | jq '.data.abuseConfidenceScore')
-            echo -e "  clean score: ${AID_SCORE}"
+            -H "Accept: application/json" | jq '{"clean score": .data.abuseConfidenceScore}' | grep -vE '{|}' | sed 's/\,//g ; s/\"//g')
+            if [ "$(echo $AID_RES | awk '{print $3}')" -gt '50' ];then
+                  echo -e "AbuseIPDB: ${RED} NOT CLEAN!\n${AID_RES}${RESET}"
+            elif [ "$(echo $AID_RES | awk '{print $3}')" -gt '0' ];then
+                  echo -e "AbuseIPDB: ${YELLOW} possibly not clean!\n${AID_RES}${RESET}"
+            elif [ "$(echo $AID_RES | awk '{print $3}')" == '0' ];then
+                  echo -e "AbuseIPDB: ${GREEN}clean :)${RESET}"
+            fi
 
-            echo -e "\nGreynoise:"
-            GREAYNOISE_SCORE=$(curl -s -H "key: ${GREAYNOISE_API_KEY}" "https://api.greynoise.io/v3/community/${IP}" | grep -vE "{|}" | sed 's/"//g ; s/,//g')
-            echo "$GREAYNOISE_SCORE"
-
-            echo -e "\nGeoIPLookup:"
-            GEOIPLOOKUP_RES=$(geoiplookup "$IP")
-            if ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $4}' | grep -E 'RU|CN|KP|IR|LB|YE|PS')" ];then
-                  echo -e " ${RED}${GEOIPLOOKUP_RES}${RESET}"
-            elif ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $4}' | grep -E 'IN|EG|UA')" ];then
-                  echo -e " ${YELLOW}${GEOIPLOOKUP_RES}${RESET}"
+            FRAUDGUARD_RES=$(curl -s -X GET -u "${FRAUDGUARD_CREDS}" "https://@api.fraudguard.io/v2/ip/${IP}" | jq '{"risk level": .risk_level}' | grep -vE '{|}' | sed 's/\,//g ; s/\"//g')
+            if [ "$(echo $FRAUDGUARD_RES | awk '{print $3}')" -gt '3' ];then
+                 echo -e "Fraudguard: ${RED}NOT CLEAN!\n${FRAUDGUARD_RES}${RESET}"
+            elif [ "$(echo $FRAUDGUARD_RES | awk '{print $3}')" -gt '1' ];then
+                 echo -e "Fraudguard: ${YELLOW} possibly not clean!\n${FRAUDGUARD_RES}${RESET}"
             else
-                  echo -e " ${GEOIPLOOKUP_RES}"
+                  echo -e "Fraudguard: ${GREEN}clean :)${RESET}"
+            fi
+
+            GEOIPLOOKUP_RES=$(geoiplookup "$IP" | awk '{$1=$2=$3=""; print $0}')
+            if ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $1}' | grep -E 'RU|CN|KP|IR|LB|YE|PS')" ];then
+                  echo -e "GeoIPLookup:${RED}${GEOIPLOOKUP_RES}${RESET}"
+            elif ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $1}' | grep -E 'IN|EG|UA')" ];then
+                  echo -e "GeoIPLookup:${YELLOW}${GEOIPLOOKUP_RES}${RESET}"
+            else
+                  echo -e "GeoIPLookup:${GEOIPLOOKUP_RES}"
             fi
       done
 }
 
 function SINGLE_IP(){
-      echo -e "results for ${IP}:\n\nVirusTotal:"
-      VT_SCORE=$(curl -s --request GET \
+      echo -e "\n${CYAN}results for ${IP}:${RESET}\n"
+      VT_RES_PARSED=$(curl -s --request GET \
       --url "https://www.virustotal.com/api/v3/ip_addresses/${IP}" \
-      --header "x-apikey: ${VIRUSTOTAL_API_KEY}" | jq '.data.attributes.last_analysis_stats' | grep -Ev '\{|\}' |sed 's/"//g ; s/,//g')
-      echo -e "${VT_SCORE}"
+      --header "x-apikey: ${VIRUSTOTAL_API_KEY}" | jq '.data.attributes.last_analysis_stats' | grep -vE '{|}' | sed 's/\,//g ; s/\"//g')
+      VT_RES_SCORE=$(echo "$VT_RES_PARSED" | grep -E 'malicious|suspicious' | awk '{print $2}' | sort | uniq)
+      if [ "$VT_RES_SCORE" != '0' ];then
+            echo -e "VirusTotal: ${RED}NOT CLEAN!\n${VT_RES_PARSED}${RESET}" 
+      else 
+            echo -e "VirusTotal: ${GREEN}clean :)${RESET}"
+      fi
 
-      echo -e "\nAbuseIPDB:"
-      AID_SCORE=$(curl -s -G https://api.abuseipdb.com/api/v2/check \
+      AID_RES=$(curl -s -G https://api.abuseipdb.com/api/v2/check \
       --data-urlencode "ipAddress=${IP}" \
       -H "Key: ${ABUSEIPDB_API_KEY}" \
-      -H "Accept: application/json" | jq '.data.abuseConfidenceScore')
-      echo -e "  clean score: ${AID_SCORE}"
-
-      echo -e "\nGreynoise:"
-      GREAYNOISE_RES=$(curl -s -H "key: ${GREAYNOISE_API_KEY}" "https://api.greynoise.io/v3/community/${IP}" | grep -vE "{|}" | sed 's/"//g ; s/,//g')
-      echo "$GREAYNOISE_RES"
-
-      echo -e "\nGeoIPLookup:"
-      GEOIPLOOKUP_RES=$(geoiplookup "$IP")
-      if ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $4}' | grep -E 'RU|CN|KP|IR|LB|YE|PS|SY')" ];then
-            echo -e " ${RED}${GEOIPLOOKUP_RES}${RESET}"
-      elif ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $4}' | grep -E 'IN|EG|UA')" ];then
-            echo -e " ${YELLOW}${GEOIPLOOKUP_RES}${RESET}"
+      -H "Accept: application/json" | jq '{"clean score": .data.abuseConfidenceScore}' | grep -vE '{|}' | sed 's/\,//g ; s/\"//g')
+      if [ "$(echo $AID_RES | awk '{print $3}')" -gt '50' ];then
+            echo -e "AbuseIPDB: ${RED}NOT CLEAN!\n${AID_RES}${RESET}"
+      elif [ "$(echo $AID_RES | awk '{print $3}')" -gt '0' ];then
+            echo -e "AbuseIPDB: ${YELLOW} possibly not clean!\n${AID_RES}${RESET}"
+      elif [ "$(echo $AID_RES | awk '{print $3}')" == '0' ];then
+            echo -e "AbuseIPDB: ${GREEN}clean :)${RESET}"
+      fi
+      
+      FRAUDGUARD_RES=$(curl -s -X GET -u "${FRAUDGUARD_CREDS}" "https://@api.fraudguard.io/v2/ip/${IP}" | jq '{"risk level": .risk_level}' | grep -vE '{|}' | sed 's/\,//g ; s/\"//g')
+      if [ "$(echo $FRAUDGUARD_RES | awk '{print $3}')" -gt '3' ];then
+            echo -e "Fraudguard: ${RED}NOT CLEAN!\n${FRAUDGUARD_RES}${RESET}"
+      elif [ "$(echo $FRAUDGUARD_RES | awk '{print $3}')" -gt '1' ];then
+            echo -e "Fraudguard: ${YELLOW} possibly not clean!\n${FRAUDGUARD_RES}${RESET}"
       else
-            echo -e " ${GEOIPLOOKUP_RES}"
+            echo -e "Fraudguard: ${GREEN}clean :)${RESET}"
+      fi
+
+      GEOIPLOOKUP_RES=$(geoiplookup "$IP" | awk '{$1=$2=$3=""; print $0}')
+      if   [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $1}' | sed 's/,//g' | grep -vE 'RU|CN|KP|IR|LB|YE|PS|SY')" ];then
+            echo -e "GeoIPLookup:${RED}${GEOIPLOOKUP_RES}${RESET}"
+      elif ! [ -z "$(echo "$GEOIPLOOKUP_RES" | awk '{print $1}' | sed 's/,//g' | grep -E 'IN|EG|UA')" ];then
+            echo -e "GeoIPLookup:${YELLOW}${GEOIPLOOKUP_RES}${RESET}"
+      else
+            echo -e "GeoIPLookup:${GEOIPLOOKUP_RES}"
       fi
 }
 
